60b0fd2d0b7221257212abd678bf45ceb114ca74
Hadri2103 <32070136+Hadri2103>
2018-05-07 14:31:28 +0200

    Object User où il manque que les fonctions utilisant la DB

--- MiniPollApp/app/src/main/java/be/lsinf1225gr12/minipoll/minipollapp/User.java
+++ MiniPollApp/app/src/main/java/be/lsinf1225gr12/minipoll/minipollapp/User.java
@@ -1,163 +1,136 @@
 package be.lsinf1225gr12.minipoll.minipollapp;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.SparseArray;
-
-import java.util.ArrayList;
-
-import be.lsinf1225gr12.minipoll.minipollapp.MySQLiteHelper;
+import java.util.List;
+import java.util.Set;
 
 public class User
 {
-    private static final String DB_COLUMN_ID = "u_id";
-    private static final String DB_COLUMN_NAME = "u_name";
-    private static final String DB_COLUMN_PASSWORD = "u_password";
-    private static final String DB_TABLE = "users";
-    /**
-     * Contient les instances déjà existantes des utilisateurs afin d'éviter de créer deux instances
-     * du même utilisateur.
-     */
-    private static SparseArray<User> userSparseArray = new SparseArray<>();
-    /**
-     * Utilisateur actuellement connecté à l'application. Correspond à null si aucun utilisateur
-     * n'est connecté.
-     */
-    private static User connectedUser = null;
-    /**
-     * Identifiant unique de l'utilisateur courant. Correspond à _id dans la base de données.
-     */
-    private final int id;
-    /**
-     * Nom (unique) de l'utilisateur courant. Correspond à u_nom dans la base de données.
-     */
-    private String name;
-    /**
-     * Mot de passe de l'utilisateur courant. Correspond à u_password dans la base de données.
-     */
+    /* Variables de la classe */
+    Set<AssociationEval> associationEval;
+    private String id;
+    private String login;
     private String password;
+    private String picture;
+    private String mail;
+    private String firstname;
+    private String name;
 
-    /**
-     * Constructeur de l'utilisateur. Initialise une instance de l'utilisateur présent dans la base
-     * de données.
-     *
-     * @note Ce constructeur est privé (donc utilisable uniquement depuis cette classe). Cela permet
-     * d'éviter d'avoir deux instances différentes d'un même utilisateur.
-     */
-    private User(int uId, String uName, String uPassword) {
+    /* Constructeurs */
+    public User(){
 
-        this.id = uId;
-        this.name = uName;
-        this.password = uPassword;
-        User.userSparseArray.put(uId, this);
     }
 
-    /**
-     * Fournit l'utilisateur actuellement connecté.
-     */
-    public static User getConnectedUser() {
-        return User.connectedUser;
+    public User(String id , String login, String password, String picture, String mail, String firstname, String name){
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.picture = picture;
+        this.mail = mail;
+        this.firstname = firstname;
+        this.name = name;
     }
 
-    /**
-     * Déconnecte l'utilisateur actuellement connecté à l'application.
-     */
-    public static void logout() {
-        User.connectedUser = null;
-    }
+    /* Getters and setters */
 
-    /**
-     * Fournit la liste des utilisateurs.
-     */
-    public static ArrayList<User> getUtilisateurs() {
-        // Récupération du  SQLiteHelper et de la base de données.
-
-        // SQLiteDatabase db = MySQLiteHelper.get().getReadableDatabase();
-
-        TestAdapter mDbHelper = new TestAdapter(MiniPollApp.getContext());
-        mDbHelper.createDatabase();
-        mDbHelper.open();
-
-        Cursor testdata = mDbHelper.getTestData();
-
-        // Colonnes à récupérer
-        String[] colonnes = {DB_COLUMN_ID, DB_COLUMN_NAME, DB_COLUMN_PASSWORD};
-
-        // Requête de selection (SELECT)
-        Cursor cursor = mDbHelper.query(DB_TABLE, colonnes, null, null, null, null, null);
-
-        // Placement du curseur sur la première ligne.
-        cursor.moveToFirst();
-
-        // Initialisation la liste des utilisateurs.
-        ArrayList<User> users = new ArrayList<>();
-
-        // Tant qu'il y a des lignes.
-        while (!cursor.isAfterLast()) {
-            // Récupération des informations de l'utilisateur pour chaque ligne.
-            int uId = cursor.getInt(0);
-            String uNom = cursor.getString(1);
-            String uPassword = cursor.getString(2);
-
-            // Vérification pour savoir s'il y a déjà une instance de cet utilisateur.
-            User user = User.userSparseArray.get(uId);
-            if (user == null) {
-                // Si pas encore d'instance, création d'une nouvelle instance.
-                user = new User(uId, uNom, uPassword);
-            }
-
-            // Ajout de l'utilisateur à la liste.
-            users.add(user);
-
-            // Passe à la ligne suivante.
-            cursor.moveToNext();
-        }
-
-        // Fermeture du curseur et de la base de données.
-        cursor.close();
-        mDbHelper.close();
-
-        return users;
-    }
-
-    /**
-     * Fournit l'id de l'utilisateur courant.
-     */
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    /**
-     * Fournit le nom de l'utilisateur courant.
-     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
     public String getName() {
         return name;
     }
 
-    /**
-     * Connecte l'utilisateur courant.
-     *
-     * @param passwordToTry le mot de passe entré.
-     *
-     * @return Vrai (true) si l'utilisateur à l'autorisation de se connecter, false sinon.
-     */
-    public boolean login(String passwordToTry) {
-        if (this.password.equals(passwordToTry)) {
-            // Si le mot de passe est correct, modification de l'utilisateur connecté.
-            User.connectedUser = this;
-            return true;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /* Functions qui check un bon identifiant */
+    public boolean connexion(String typedLogin, String typedPassword){
+        return(this.login.equals(typedLogin) && this.password.equals(typedPassword));
+    }
+
+    public boolean checkLogin(String typedLogin){
+        return (this.login.equals(typedLogin));
+    }
+
+    public boolean checkMail(String typedMail){
+        return (this.mail.equals(typedMail));
+    }
+
+    /* Retourne la liste des utilisateurs qui ont envoyé une demande
+    * A FAIRE */
+    public List<User> notifications(){
+        List<User> list = null;
+        return list;
+    }
+
+    /* Fonction qui envoie une demande à un ami
+    * Renvoie true si la demande a bien été envoyée
+    * Renvoie false si une erreur s'est produite (ex: user n'existe pas, déjà ami, ...)
+    * A FAIRE */
+    public boolean addFriend(User user){
         return false;
     }
 
-    /**
-     * Fournit une représentation textuelle de l'utilisateur courant. (Ici le nom)
-     *
-     * @note Cette méthode est utilisée par l'adaptateur ArrayAdapter afin d'afficher la liste des
-     * utilisateurs. (Voir LoginActivity).
-     */
-    public String toString() {
-        return getName();
+    /* Fonction qui renvoie la liste d'amis de l'user
+    * A FAIRE */
+    public List<User> fiendList(){
+        List<User> list = null;
+        return list;
     }
 
+    /* Aucune idée de à quoi ça sert ça retourne rien
+    * A FAIRE */
+    public void viewFriendList(List<User> list){
+
+    }
+
+    /* A FAIRE */
+    public void computePollResult(PollAbstract pollAbstract){
+
+    }
 }
