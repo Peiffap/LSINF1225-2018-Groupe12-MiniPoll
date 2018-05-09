e6356c4ec69e2597604832ee766fe133faa930c1
Baptistedemu <baptiste.demuysere@student.uclouvain.be>
2018-05-10 01:12:10 +0200

    Revert "MCQAnswer"
    This reverts commit 0824e1c68215c03177ed4d6a5b8c6f69a1df2b9c.

--- MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/model/Question.java
+++ MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/model/Question.java
@@ -7,24 +7,10 @@ package be.lsinf1225gr12.minipoll.minipoll.model;
 public class Question {
     private String title;
     private int rightAnswerPosition;
-    private int qpos;
 
-    public Question(String title, int rightAnswerPosition, int qpos)
+    public Question(String title, int rightAnswerPosition)
     {
         this.title = title;
         this.rightAnswerPosition = rightAnswerPosition;
-        this.qpos=qpos;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public int getRightAnswerPosition() {
-        return rightAnswerPosition;
-    }
-
-    public int getQpos() {
-        return qpos;
     }
 }
