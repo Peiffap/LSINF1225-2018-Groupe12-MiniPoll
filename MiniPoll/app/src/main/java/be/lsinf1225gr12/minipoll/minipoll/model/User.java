23a91218fd4995f64a2862bbd3dae4e5a9793556
Hadri2103 <32070136+Hadri2103>
2018-05-09 17:41:27 +0200

    Added une fonction en plus et les descriptions de toutes les fonctions

--- MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/model/User.java
+++ MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/model/User.java
@@ -326,6 +326,11 @@ public class User {
         db.close();
     }
 
+    /**
+     * Fonction qui permet d'ajouter un ami
+     * @param sender personne qui envoie l'invitation
+     * @param receiver personne qui reçoit l'invitation
+     */
     public static void addFriend(User sender, User receiver){
         int idSender = sender.getId();
         int idReceiver = receiver.getId();
@@ -343,12 +348,22 @@ public class User {
         db.close();
     }
 
+    /**
+     * Fonction qui permet de supprimer un ami
+     * @param user Personne qui supprime son ami
+     * @param friendToRemove L'ami qui est supprimé
+     */
     public static void removeFriend(User user, User friendToRemove){
         int idUser = user.getId();
         int idRemove = friendToRemove.getId();
 
     }
 
+    /**
+     * Fonction qui permet d'accepter une demande d'ami
+     * @param user personne qui accepte la demande
+     * @param friendToAccept personne qui lui a envoyé la demande
+     */
     public static void acceptFriend(User user, User friendToAccept){
         int idUser = user.getId();
         int idAccept = friendToAccept.getId();
@@ -364,6 +379,86 @@ public class User {
             //erreur dans l'ajout, suppression
         }
         db.close();
+    }
+
+    /**
+     * Fonction qui renvoie la liste des utilisateurs ayant envoyé une demande à user
+     * @param utilisateur l'utilisateur dont on regarde la liste de demandes en ami
+     * @return liste d'utilisateurs lui envoyant une demande
+     */
+    public static ArrayList<User> getDemands(User utilisateur){
+        int thisId = utilisateur.getId();
+
+        // Récupération du  SQLiteHelper et de la base de données.
+        SQLiteDatabase db = MySQLiteHelper.get().getReadableDatabase();
+
+        String selection = MySQLiteHelper.getKeyFriendrelationStatus() + " = ? AND " + MySQLiteHelper.getKeyFriendrelationReceiver() + " = ?";
+        String[] selectionArgs = new String[]{"Pending", String.valueOf(thisId)};
+
+        String[] colonnes = {MySQLiteHelper.getKeyFriendrelationSender()};
+        Cursor cursor = db.query(MySQLiteHelper.getTableFriendrelation(), colonnes, selection, selectionArgs, null, null, null);
+
+        // Placement du curseur sur la première ligne.
+        cursor.moveToFirst();
+
+        ArrayList<String> ids = new ArrayList<>();
+
+        // Tant qu'il y a des lignes.
+        while (!cursor.isAfterLast()) {
+            // Récupération des informations de l'utilisateur pour chaque ligne.
+            int id = cursor.getInt(cursor.getColumnIndex(MySQLiteHelper.getKeyFriendrelationReceiver()));
+
+            // Ajout de l'utilisateur à la liste.
+            ids.add(String.valueOf(id));
+
+            // Passe à la ligne suivante.
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+
+        String selection3 = MySQLiteHelper.getKeyUserId() + " = ?";
+        String[] selectionArgs3 = new String[ids.size()];
+        selectionArgs3 = ids.toArray(selectionArgs3);
+
+        String[] colonnes3 = {MySQLiteHelper.getKeyUserId(), MySQLiteHelper.getKeyUserSurname(), MySQLiteHelper.getKeyUserFirstname(),MySQLiteHelper.getKeyUserLogin(),MySQLiteHelper.getKeyUserPassword(),MySQLiteHelper.getKeyUserMail(),MySQLiteHelper.getKeyUserPicture(),MySQLiteHelper.getKeyUserBestfriend()};
+        Cursor cursor3 = db.query(MySQLiteHelper.getTableUser(), colonnes3, selection3, selectionArgs3, null, null, null);
+
+        // Placement du curseur sur la première ligne.
+        cursor3.moveToFirst();
+
+        // Initialisation la liste des utilisateurs.
+        ArrayList<User> users = new ArrayList<>();
+
+        // Tant qu'il y a des lignes.
+        while (!cursor3.isAfterLast()) {
+            // Récupération des informations de l'utilisateur pour chaque ligne.
+            int id = cursor3.getInt(cursor3.getColumnIndex(MySQLiteHelper.getKeyUserId()));
+            String name = cursor3.getString(cursor3.getColumnIndex(MySQLiteHelper.getKeyUserSurname()));
+            String firstname = cursor3.getString(cursor3.getColumnIndex(MySQLiteHelper.getKeyUserFirstname()));
+            String login = cursor3.getString(cursor3.getColumnIndex(MySQLiteHelper.getKeyUserLogin()));
+            String password = cursor3.getString(cursor3.getColumnIndex(MySQLiteHelper.getKeyUserPassword()));
+            String mail = cursor3.getString(cursor3.getColumnIndex(MySQLiteHelper.getKeyUserMail()));
+            String picture = cursor3.getString(cursor3.getColumnIndex(MySQLiteHelper.getKeyUserPicture()));
+            String bestFriend = cursor3.getString(cursor3.getColumnIndex(MySQLiteHelper.getKeyUserBestfriend()));
+
+            User user = new User(id, login, password, picture, mail, firstname, name, bestFriend);
+
+            // Ajout de l'utilisateur à la liste.
+            users.add(user);
+
+            // Passe à la ligne suivante.
+            cursor3.moveToNext();
+        }
+
+        // Fermeture du curseur et de la base de données.
+        cursor3.close();
+
+        // Fermeture du curseur et de la base de données.
+
+        db.close();
+
+        return users;
     }
 }
 
