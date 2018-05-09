5e360b3e0433cd1a3aeb80b850a6f45da3560c5b
augustindelecluse <36609292+augustindelecluse@users.noreply.github.com>
2018-05-09 12:11:40 +0200

    MySQLiteHelper

---
+++ MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/MySQLiteHelper.java
@@ -0,0 +1,384 @@
+package be.lsinf1225gr12.minipoll.minipoll;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+import android.os.StrictMode;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+//nom des classes ressources
+/*
+import be.lsinf1225gr12.minipoll.minipollapp.AssociationEval;
+import be.lsinf1225gr12.minipoll.minipollapp.AssociationMCQ;
+import be.lsinf1225gr12.minipoll.minipollapp.MCQ;
+import be.lsinf1225gr12.minipoll.minipollapp.MCQAnswer;
+import be.lsinf1225gr12.minipoll.minipollapp.Poll;
+import be.lsinf1225gr12.minipoll.minipollapp.PollAbstract;
+import be.lsinf1225gr12.minipoll.minipollapp.PollAnswer;
+import be.lsinf1225gr12.minipoll.minipollapp.Question;
+import be.lsinf1225gr12.minipoll.minipollapp.User;
+*/
+
+/*
+classe générale pour accès à la DB.
+Pour plus de détails sur le fonctionnement, consulter https://github.com/codepath/android_guides/wiki/Local-Databases-with-SQLiteOpenHelper
+*/
+public class MySQLiteHelper extends SQLiteOpenHelper {
+    // Database Info
+    private static final String DATABASE_NAME = "Database";
+    private static final int DATABASE_VERSION = 2;
+
+    // Table Names
+    private static final String TABLE_ANSWERPOLL = "answerpoll";
+    private static final String TABLE_ANSWERQUESTION = "answerquestion";
+    private static final String TABLE_CHOICEPOLL = "pollchoice";
+    private static final String TABLE_CHOICEQUESTION = "possiblechoice";
+    private static final String TABLE_FRIENDRELATION = "friendrelation";
+    private static final String TABLE_MCQ = "mcq";
+    private static final String TABLE_PARTICIPATIONPOLL = "pollparticipation";
+    private static final String TABLE_PARTICIPATIONQUESTION = "questionparticipation";
+    private static final String TABLE_POLL = "poll";
+    private static final String TABLE_QUESTION = "question";
+    private static final String TABLE_USER = "user";
+
+    //AnswerPoll Table Columns
+    private static final String KEY_ANSWERPOLL_USER = "USER";
+    private static final String KEY_ANSWERPOLL_AUTHOR = "AUTHOR";
+    private static final String KEY_ANSWERPOLL_DATE = "DATE";
+    private static final String KEY_ANSWERPOLL_CHOICE = "CHOICE";
+    private static final String KEY_ANSWERPOLL_SCORE = "SCORE";
+
+    //AnswerQuestion Table Columns
+    private static final String KEY_ANSWERQUESTION_AUTHOR = "AUTHOR";
+    private static final String KEY_ANSWERQUESTION_DATE = "DATE";
+    private static final String KEY_ANSWERQUESTION_POSITION = "POSITION";
+    private static final String KEY_ANSWERQUESTION_QUESTIONPOSITION = "QPOS";
+    private static final String KEY_ANSWERQUESTION_USER = "USER";
+
+    //ChoicePoll Table Columns
+    private static final String KEY_CHOICEPOLL_AUTHOR = "AUTHOR";
+    private static final String KEY_CHOICEPOLL_DATE = "DATE";
+    private static final String KEY_CHOICEPOLL_TEXT = "CHOICE";
+    private static final String KEY_CHOICEPOLL_POSITION = "POSITION";
+
+    //ChoiceQuestion Table Columns
+    private static final String KEY_CHOICEQUESTION_POSITION = "POSITION";
+    private static final String KEY_CHOICEQUESTION_AUTHOR = "AUTHOR";
+    private static final String KEY_CHOICEQUESTION_DATE = "DATE";
+    private static final String KEY_CHOICEQUESTION_TEXT = "TEXT";
+    private static final String KEY_CHOICEQUESTION_QUESTIONPOSITION = "QPOS";
+
+    //FriendRelation Table Columns
+    private static final String KEY_FRIENDRELATION_SENDER = "SENDER";
+    private static final String KEY_FRIENDRELATION_RECEIVER = "RECEIVER";
+    private static final String KEY_FRIENDRELATION_STATUS = "STATUS";
+
+    //MCQ Table Columns
+    private static final String KEY_MCQ_AUTHOR = "AUTHOR";
+    private static final String KEY_MCQ_DATE = "DATE";
+    private static final String KEY_MCQ_TITLE = "NAME";
+    private static final String KEY_MCQ_FORMAT = "FORMAT";
+    private static final String KEY_MCQ_ISCLOSED = "ISCLOSED";
+    private static final String KEY_MCQ_NUMBERQUESTION = "NUMBERQUESTION";
+
+    //ParticipationPoll Table Columns
+    private static final String KEY_PARTICIPATIONPOLL_USER = "USER";
+    private static final String KEY_PARTICIPATIONPOLL_AUTHOR = "AUTHOR";
+    private static final String KEY_PARTICIPATIONPOLL_DATE = "DATE";
+
+    //ParticipationQuestion Table Columns
+    private static final String KEY_PARTICIPATIONQUESTION_USER = "USER";
+    private static final String KEY_PARTICIPATIONQUESTION_AUTHOR = "AUTHOR";
+    private static final String KEY_PARTICIPATIONQUESTION_DATE = "DATE";
+
+    //Poll Table Columns
+    private static final String KEY_POLL_AUTHOR = "AUTHOR";
+    private static final String KEY_POLL_DATE = "DATE";
+    private static final String KEY_POLL_TITLE = "TITLE";
+    private static final String KEY_POLL_QUESTION = "QUESTION";
+    private static final String KEY_POLL_FORMAT = "FORMAT";
+    private static final String KEY_POLL_NUMBERTOP = "NBRTOP";
+    private static final String KEY_POLL_NUMBERCHOICE = "NBRCHOICE";
+    private static final String KEY_POLL_ISPOLL = "ISPOLL";
+    private static final String KEY_POLL_ISCLOSED = "ISCLOSED";
+
+    //Question Table Columns
+    private static final String KEY_QUESTION_AUTHOR = "AUTHOR";
+    private static final String KEY_QUESTION_DATE = "DATE";
+    private static final String KEY_QUESTION_DESCRIPTION = "DESCRIPTION";
+    private static final String KEY_QUESTION_POSITION = "POSITION";
+    private static final String KEY_QUESTION_RIGHTANSWER = "RIGHTANSWER";
+
+    // User Table Columns
+    private static final String KEY_USER_ID = "ID";
+    private static final String KEY_USER_SURNAME = "NAME";
+    private static final String KEY_USER_FIRSTNAME = "FIRSTNAME";
+    private static final String KEY_USER_LOGIN = "LOGIN";
+    private static final String KEY_USER_PASSWORD = "PASSWORD";
+    private static final String KEY_USER_MAIL = "MAIL";
+    private static final String KEY_USER_PICTURE = "PICTURE";
+    private static final String KEY_USER_BESTFRIEND = "BESTFRIEND";
+
+    private static MySQLiteHelper sInstance;
+
+    public static synchronized MySQLiteHelper getInstance(Context context) {
+        // Use the application context, which will ensure that you
+        // don't accidentally leak an Activity's context.
+        // See this article for more information: http://bit.ly/6LRzfx
+        if (sInstance == null) {
+            sInstance = new MySQLiteHelper(context.getApplicationContext());
+        }
+        return sInstance;
+    }
+
+    /**
+     * Constructor should be private to prevent direct instantiation.
+     * Make a call to the static method "getInstance()" instead.
+     */
+    private MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Called when the database connection is being configured.
+    // Configure database settings for things like foreign key support, write-ahead logging, etc.
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        super.onConfigure(db);
+    }
+
+    // Called when the database is created for the FIRST time.
+    // If a database already exists on disk with the same DATABASE_NAME, this method will NOT be called.
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String CREATE_ANSWERPOLL_TABLE = "CREATE TABLE " + TABLE_ANSWERPOLL +
+                "(\n" +
+                KEY_ANSWERPOLL_USER + " int not null references " + TABLE_USER + " (" + KEY_USER_ID + "),\n" +
+                KEY_ANSWERPOLL_AUTHOR + " int not null,\n" +
+                KEY_ANSWERPOLL_DATE + " datetime not null,\n" +
+                KEY_ANSWERPOLL_CHOICE + " int not null,\n" +
+                KEY_ANSWERPOLL_SCORE + " int not null,\n" +
+                "foreign key (\n" +
+                KEY_ANSWERPOLL_AUTHOR + ",\n" +
+                KEY_ANSWERPOLL_DATE + ",\n" +
+                KEY_ANSWERPOLL_CHOICE + "\n" +
+                ")\n" +
+                "references " + TABLE_CHOICEPOLL + " (\n" +
+                KEY_CHOICEPOLL_AUTHOR + ",\n" +
+                KEY_CHOICEPOLL_DATE + ",\n" +
+                KEY_CHOICEPOLL_POSITION +
+                ")\n" +
+                ")";
+
+        String CREATE_ANSWERQUESTION_TABLE = "CREATE TABLE " + TABLE_ANSWERQUESTION +
+                "(\n" +
+                KEY_ANSWERQUESTION_AUTHOR + " int not null,\n" +
+                KEY_ANSWERQUESTION_DATE + " datetime not null,\n" +
+                KEY_ANSWERQUESTION_POSITION + " int not null,\n" +
+                KEY_ANSWERQUESTION_QUESTIONPOSITION + " int not null,\n" +
+                KEY_ANSWERQUESTION_USER + " int not null references " + TABLE_USER + " (" + KEY_USER_ID + "),\n" +
+                "foreign key (\n" +
+                KEY_ANSWERQUESTION_AUTHOR + ",\n" +
+                KEY_ANSWERQUESTION_DATE + ",\n" +
+                KEY_ANSWERQUESTION_POSITION + ",\n" +
+                KEY_ANSWERQUESTION_QUESTIONPOSITION + "\n" +
+                ")\n" +
+                "references " + TABLE_CHOICEQUESTION + " (\n" +
+                KEY_CHOICEQUESTION_AUTHOR + ",\n" +
+                KEY_CHOICEQUESTION_DATE + ",\n" +
+                KEY_CHOICEQUESTION_POSITION + ",\n" +
+                KEY_CHOICEQUESTION_QUESTIONPOSITION +
+                ")\n" +
+                ")";
+
+        String CREATE_CHOICEPOLL_TABLE = "CREATE TABLE " + TABLE_CHOICEPOLL +
+                "(\n" +
+                KEY_CHOICEPOLL_AUTHOR + " int not null,\n" +
+                KEY_CHOICEPOLL_DATE + " datetime not null,\n" +
+                KEY_CHOICEPOLL_TEXT + " text not null,\n" +
+                KEY_CHOICEPOLL_POSITION + " int not null,\n" +
+                "primary key (\n" +
+                KEY_CHOICEPOLL_AUTHOR + ",\n" +
+                KEY_CHOICEPOLL_DATE + ",\n" +
+                KEY_CHOICEPOLL_POSITION +
+                "),\n" +
+                "foreign key (\n" +
+                KEY_CHOICEPOLL_AUTHOR + ",\n" +
+                KEY_CHOICEPOLL_DATE + ",\n" +
+                ")\n" +
+                "references " + TABLE_POLL + " (" +
+                KEY_POLL_AUTHOR + ",\n" +
+                KEY_POLL_DATE +
+                ")\n" +
+                ")";
+
+        String CREATE_CHOICEQUESTION_TABLE = "CREATE TABLE " + TABLE_CHOICEQUESTION +
+                "(\n" +
+                KEY_CHOICEQUESTION_POSITION + " int not null,\n" +
+                KEY_CHOICEQUESTION_AUTHOR + " int not null,\n" +
+                KEY_CHOICEQUESTION_DATE + " datetime not null,\n" +
+                KEY_CHOICEQUESTION_TEXT + " text not null,\n" +
+                KEY_CHOICEQUESTION_QUESTIONPOSITION + " int not null,\n" +
+                "primary key (\n" +
+                KEY_CHOICEQUESTION_POSITION + ",\n" +
+                KEY_CHOICEQUESTION_AUTHOR + ",\n" +
+                KEY_CHOICEQUESTION_DATE + ",\n" +
+                KEY_CHOICEQUESTION_QUESTIONPOSITION + "\n" +
+                "),\n" +
+                "foreign key (\n" +
+                KEY_CHOICEQUESTION_POSITION + ",\n" +
+                KEY_CHOICEQUESTION_AUTHOR + ",\n" +
+                KEY_CHOICEQUESTION_DATE + "\n" +
+                ")\n" +
+                "references " + TABLE_QUESTION + " (" +
+                KEY_QUESTION_POSITION + ",\n" +
+                KEY_QUESTION_AUTHOR + ",\n" +
+                KEY_QUESTION_DATE +
+                ")\n" +
+                ")";
+
+        String CREATE_FRIENDRELATION_TABLE = "CREATE TABLE " + TABLE_FRIENDRELATION +
+                "(\n" +
+                KEY_FRIENDRELATION_SENDER + " int references " + TABLE_USER + " (" + KEY_USER_ID + ") not null,\n" +
+                KEY_FRIENDRELATION_SENDER + " int references " + TABLE_USER + " (" + KEY_USER_ID + ") not null,\n" +
+                KEY_FRIENDRELATION_STATUS + " text not null\n" +
+                ")";
+
+        String CREATE_MCQ_TABLE = "CREATE TABLE " + TABLE_MCQ +
+                "(\n" +
+                KEY_MCQ_AUTHOR + " int references " + TABLE_USER + " (" + KEY_USER_ID + ") not null,\n" +
+                KEY_MCQ_DATE + " datetime not null,\n" +
+                KEY_MCQ_TITLE + " text not null,\n" +
+                KEY_MCQ_FORMAT + " text not null,\n" +
+                KEY_MCQ_ISCLOSED + " boolean not null,\n" +
+                KEY_MCQ_NUMBERQUESTION + " int not null,\n" +
+                "primary key (" +
+                KEY_MCQ_AUTHOR + ",\n" +
+                KEY_MCQ_DATE + "\n" +
+                ")\n" +
+                ")";
+
+        String CREATE_PARTICIPATIONPOLL_TABLE = "CREATE TABLE " + TABLE_PARTICIPATIONPOLL +
+                "(\n" +
+                KEY_PARTICIPATIONPOLL_USER + " int not null references " + TABLE_USER + " (" + KEY_USER_ID + "),\n" +
+                KEY_PARTICIPATIONPOLL_AUTHOR + " int not null,\n" +
+                KEY_PARTICIPATIONPOLL_DATE + " datetime not null,\n" +
+                "foreign key (" +
+                KEY_PARTICIPATIONPOLL_AUTHOR + ",\n" +
+                KEY_PARTICIPATIONPOLL_DATE + "\n" +
+                ")" +
+                "references" + TABLE_POLL + " (" +
+                KEY_POLL_AUTHOR + ",\n" +
+                KEY_POLL_DATE +
+                ")\n" +
+                ")";
+
+        String CREATE_PARTICIPATIONQUESTION_TABLE = "CREATE TABLE " + TABLE_PARTICIPATIONQUESTION +
+                "(\n" +
+                KEY_PARTICIPATIONQUESTION_USER + " int not null references " + TABLE_USER + " (" + KEY_USER_ID + "),\n" +
+                KEY_PARTICIPATIONQUESTION_AUTHOR + " int not null,\n" +
+                KEY_PARTICIPATIONQUESTION_DATE + " datetime not null,\n" +
+                "foreign key (" +
+                KEY_PARTICIPATIONQUESTION_AUTHOR + ",\n" +
+                KEY_PARTICIPATIONQUESTION_DATE + "\n" +
+                ")\n" +
+                "references " + TABLE_MCQ + " (" +
+                KEY_MCQ_AUTHOR + ",\n" +
+                KEY_MCQ_DATE +
+                ")\n" +
+                ")";
+
+        String CREATE_POLL_TABLE = "CREATE TABLE " + TABLE_POLL +
+                "(\n" +
+                KEY_POLL_AUTHOR + " int references " + TABLE_USER + " (" + KEY_USER_ID + ") not null,\n" +
+                KEY_POLL_DATE + " datetime not null,\n" +
+                KEY_POLL_TITLE + " text not null,\n" +
+                KEY_POLL_QUESTION + " text not null,\n" +
+                KEY_POLL_FORMAT + " text not null,\n" +
+                KEY_POLL_NUMBERTOP + " int not null,\n" +
+                KEY_POLL_NUMBERCHOICE + " int not null,\n" +
+                KEY_POLL_ISPOLL + " boolean not null,\n" +
+                KEY_POLL_ISCLOSED + " boolean not null,\n" +
+                "primary key (" +
+                KEY_POLL_AUTHOR + ",\n" +
+                KEY_POLL_DATE + "\n" +
+                ")\n" +
+                ")";
+
+        String CREATE_QUESTION_TABLE = "CREATE TABLE " + TABLE_QUESTION +
+                "(\n" +
+                KEY_QUESTION_AUTHOR + " int not null,\n" +
+                KEY_QUESTION_DATE + " datetime not null,\n" +
+                KEY_QUESTION_DESCRIPTION + " text not null,\n" +
+                KEY_QUESTION_POSITION + " int not null,\n" +
+                KEY_QUESTION_RIGHTANSWER + " int not null,\n" +
+                "primary key (" +
+                KEY_QUESTION_AUTHOR + ",\n" +
+                KEY_QUESTION_DATE + ",\n" +
+                KEY_QUESTION_POSITION + ",\n" +
+                "),\n" +
+                "foreign key (" +
+                KEY_QUESTION_AUTHOR + ",\n" +
+                KEY_QUESTION_DATE + "\n" +
+                ")\n" +
+                "references " + TABLE_MCQ + "(" +
+                KEY_MCQ_AUTHOR + ",\n" +
+                KEY_MCQ_DATE +
+                ")\n" +
+                ")";
+
+        String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER +
+                "(\n" +
+                KEY_USER_ID + " int not null unique primary key,\n" +
+                KEY_USER_LOGIN + " text null,\n" +
+                KEY_USER_FIRSTNAME + " text not null,\n" +
+                KEY_USER_SURNAME + " text not null,\n" +
+                KEY_USER_PASSWORD + " text not null,\n" +
+                KEY_USER_MAIL + " text not null,\n" +
+                KEY_USER_PICTURE + " text,\n" +
+                KEY_USER_BESTFRIEND + " text references " + TABLE_USER + " (" + KEY_USER_ID + ")\n" +
+                ")";
+
+        db.execSQL(CREATE_USER_TABLE);
+        db.execSQL(CREATE_ANSWERPOLL_TABLE);
+        db.execSQL(CREATE_ANSWERQUESTION_TABLE);
+        db.execSQL(CREATE_CHOICEPOLL_TABLE);
+        db.execSQL(CREATE_CHOICEQUESTION_TABLE);
+        db.execSQL(CREATE_FRIENDRELATION_TABLE);
+        db.execSQL(CREATE_MCQ_TABLE);
+        db.execSQL(CREATE_PARTICIPATIONPOLL_TABLE);
+        db.execSQL(CREATE_PARTICIPATIONQUESTION_TABLE);
+        db.execSQL(CREATE_POLL_TABLE);
+        db.execSQL(CREATE_QUESTION_TABLE);
+    }
+
+    // Called when the database needs to be upgraded.
+    // This method will only be called if a database already exists on disk with the same DATABASE_NAME,
+    // but the DATABASE_VERSION is different than the version of the database that exists on disk.
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion != newVersion) {
+            // Simplest implementation is to drop all old tables and recreate them
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_ANSWERPOLL);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_ANSWERQUESTION);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_CHOICEPOLL);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_CHOICEQUESTION);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_FRIENDRELATION);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_MCQ);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_PARTICIPATIONPOLL);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_PARTICIPATIONQUESTION);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_POLL);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_QUESTION);
+            onCreate(db);
+        }
+    }
+}
