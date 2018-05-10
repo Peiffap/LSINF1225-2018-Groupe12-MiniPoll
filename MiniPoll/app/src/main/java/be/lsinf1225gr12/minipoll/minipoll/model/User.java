a9b70cef4ff526d869e503759f42ea0b023ccebb
Hadri2103 <32070136+Hadri2103>
2018-05-11 01:09:50 +0200

    Rectification de toutes les methodes set()

--- MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/model/User.java
+++ MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/model/User.java
@@ -86,9 +86,10 @@ public class User {
         SQLiteDatabase db = MySQLiteHelper.get().getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(MySQLiteHelper.getKeyUserPassword(),password);
-        db.update(MySQLiteHelper.getTableUser(), cv, null, null);
-        long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
-        if (result==-1)
+        String[] whereArgs = new String[]{String.valueOf(this.id)};
+        int result = db.update(MySQLiteHelper.getTableUser(), cv, MySQLiteHelper.getKeyUserId() + " = ?", whereArgs);
+        //long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
+        if (result<=0)
         {
             MiniPollApp.notifyShort(R.string.my_error);
         }
@@ -104,9 +105,10 @@ public class User {
         SQLiteDatabase db = MySQLiteHelper.get().getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(MySQLiteHelper.getKeyUserPicture(),picture);
-        db.update(MySQLiteHelper.getTableUser(), cv, null, null);
-        long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
-        if (result==-1)
+        String[] whereArgs = new String[]{String.valueOf(this.id)};
+        int result = db.update(MySQLiteHelper.getTableUser(), cv, MySQLiteHelper.getKeyUserId() + " = ?", whereArgs);
+        //long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
+        if (result<=0)
         {
             MiniPollApp.notifyShort(R.string.my_error);
         }
@@ -122,9 +124,10 @@ public class User {
         SQLiteDatabase db = MySQLiteHelper.get().getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(MySQLiteHelper.getKeyUserMail(),mail);
-        db.update(MySQLiteHelper.getTableUser(), cv, null, null);
-        long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
-        if (result==-1)
+        String[] whereArgs = new String[]{String.valueOf(this.id)};
+        int result = db.update(MySQLiteHelper.getTableUser(), cv, MySQLiteHelper.getKeyUserId() + " = ?", whereArgs);
+        //long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
+        if (result<=0)
         {
             MiniPollApp.notifyShort(R.string.my_error);
         }
@@ -140,9 +143,10 @@ public class User {
         SQLiteDatabase db = MySQLiteHelper.get().getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(MySQLiteHelper.getKeyUserFirstname(),firstname);
-        db.update(MySQLiteHelper.getTableUser(), cv, null, null);
-        long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
-        if (result==-1)
+        String[] whereArgs = new String[]{String.valueOf(this.id)};
+        int result = db.update(MySQLiteHelper.getTableUser(), cv, MySQLiteHelper.getKeyUserId() + " = ?", whereArgs);
+        //long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
+        if (result<=0)
         {
             MiniPollApp.notifyShort(R.string.my_error);
         }
@@ -158,9 +162,10 @@ public class User {
         SQLiteDatabase db = MySQLiteHelper.get().getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(MySQLiteHelper.getKeyUserSurname(),name);
-        db.update(MySQLiteHelper.getTableUser(), cv, null, null);
-        long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
-        if (result==-1)
+        String[] whereArgs = new String[]{String.valueOf(this.id)};
+        int result = db.update(MySQLiteHelper.getTableUser(), cv, MySQLiteHelper.getKeyUserId() + " = ?", whereArgs);
+        //long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
+        if (result<=0)
         {
             MiniPollApp.notifyShort(R.string.my_error);
         }
@@ -180,9 +185,10 @@ public class User {
         SQLiteDatabase db = MySQLiteHelper.get().getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(MySQLiteHelper.getKeyUserBestfriend(),String.valueOf(bestFriendId));
-        db.update(MySQLiteHelper.getTableUser(), cv, null, null);
-        long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
-        if (result==-1)
+        String[] whereArgs = new String[]{String.valueOf(this.id)};
+        int result = db.update(MySQLiteHelper.getTableUser(), cv, MySQLiteHelper.getKeyUserId() + " = ?", whereArgs);
+        //long result = db.insert(MySQLiteHelper.getTableUser(), null, cv);
+        if (result<=0)
         {
             MiniPollApp.notifyShort(R.string.my_error);
         }
