ff2762f5145c63e14753d7ee104a3bc81a2f8b2d
Guillian <guillian.steppe@student.uclouvain.be>
2018-05-10 21:47:31 +0200

    CreateSondageActivity

---
+++ MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/activity/PollEditActivity.java
@@ -0,0 +1,13 @@
+package be.lsinf1225gr12.minipoll.minipoll.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PollEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poll_edit);
+    }
+}
