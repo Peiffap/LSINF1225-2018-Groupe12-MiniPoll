b019a62c66c9cfb928d25d70cb5d8356b6eee8c0
augustindelecluse <36609292+augustindelecluse@users.noreply.github.com>
2018-05-10 23:19:34 +0200

    Test liste user (non fonctionnel)

--- MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/activity/ChooseFriendActivity.java
+++ MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/activity/ChooseFriendActivity.java
@@ -1,59 +1,98 @@
 package be.lsinf1225gr12.minipoll.minipoll.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import be.lsinf1225gr12.minipoll.minipoll.MiniPollApp;
 import be.lsinf1225gr12.minipoll.minipoll.R;
 import be.lsinf1225gr12.minipoll.minipoll.adapter.ChooseFriendAdapter;
 import be.lsinf1225gr12.minipoll.minipoll.model.User;
 
-public class ChooseFriendActivity extends AppCompatActivity
+public class ChooseFriendActivity extends Activity implements OnItemClickListener
 {
 
     public static final String FriendMemory = "be.lsinf1225gr12.minipoll.minipoll.activity.FriendMemory";
 
-    TextView noF;
-    ListView LVC;
-    Button BC;
+    private ArrayList<User> user;
+    private ChooseFriendAdapter chooseFriendAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_choose_friend);
-        ListView LV = findViewById(R.id.LVC);
-        if (User.getFriends(User.getConnectedUser()).size()==0){
-            LVC.setVisibility(View.GONE);
-            BC = (Button) findViewById(R.id.BC);
-            BC.setVisibility(View.GONE);
-            noF = (TextView) findViewById(R.id.noF);
-            noF.setText("Vous n'avez pas d'amis, vous devriez vous en inquiéter");
-        }
-        String f = "";
-        for(int i=0; i<User.getFriends(User.getConnectedUser()).size();i++){
-            f+=(User.getFriends(User.getConnectedUser()).get(i).getLogin()+"-");
-        }
-        String[] friendList= f.split("-");
-        ChooseFriendAdapter adapter = new ChooseFriendAdapter(this, friendList);
-        LV.setAdapter(adapter);
+
+        // Chargement des éléments à afficher dans la variable de classe songs
+        loadUser();
+
+        ListView myListView = findViewById(R.id.show_listView);
+
+        // Création de l'adapter pour faire la liaison entre les données (songs) et
+        // l'affichage de chaque ligne de la liste.
+        chooseFriendAdapter = new ChooseFriendAdapter(this, user);
+        myListView.setAdapter(chooseFriendAdapter);
+
+        // Indique que le clic d'un élément de la liste doit appeler la méthode onItemClick d
+        // cette classe (this).
+        myListView.setOnItemClickListener(this);
+
     }
-    public void next (View v){
-        Intent pIntent = getIntent();
-        int Type = pIntent.getIntExtra(CreateActivity.TypeMemory,0);
-        String[] Chosen = {"PlaceHolder1","PlaceHolder2"}; //put chosen friends
-        Intent intent;
-        if(Type==0){
-            intent = new Intent(this,CreateSondageActivity.class);
+
+    private void loadUser() {
+
+        // Récupération de la requête de recherche.
+        // Si aucune requête n'a été passée lors de la création de l'activité, searchQuery sera null.
+        String searchQuery = getIntent().getStringExtra("searchQuery");
+
+        if (searchQuery == null) {
+            //user = User.getConnectedUser().getFriends();
+        } else {
+            //user = User.getConnectedUser().getFriends();
         }
-        else{
-            intent = new Intent(this,CreateQuestionnaireActivity.class);
+
+        // S'il n'y a aucun éléments dans la liste, il faut afficher un message. Ce message est différent
+        // s'il y avait une requête de recherche (message du type "Aucun résultat trouvé") ou si
+        // l'utilisateur vient directement du menu principal et veut tout afficher (message du type
+        // "Aucun élément n'est présent dans votre collection).
+        if (user.isEmpty()) {
+            if (searchQuery == null) {
+                MiniPollApp.notifyShort(R.string.list_error);
+            } else {
+                MiniPollApp.notifyShort(R.string.list_error);
+            }
+            // Cloture de l'activité d'affichage de la liste (car liste vide). Retour à l'écran
+            // précédent.
+            finish();
         }
-        intent.putExtra(FriendMemory,Chosen);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // La liste des éléments est ici rechargées car en cas de modification d'un élément, l'ordre
+        // a peut-être changé.
+
+        loadUser();
+
+        chooseFriendAdapter.setUser(user);
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(this, MainActivity.class);
+        // L'id de l'élément de collection est passé en argument afin que la vue de détails puisse
+        // récupérer celui-ci.
+        intent.putExtra("s_id", user.get(position).getId());
         startActivity(intent);
     }
 }
--- MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/adapter/ChooseFriendAdapter.java
+++ MiniPoll/app/src/main/java/be/lsinf1225gr12/minipoll/minipoll/adapter/ChooseFriendAdapter.java
@@ -1,31 +1,80 @@
 package be.lsinf1225gr12.minipoll.minipoll.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import be.lsinf1225gr12.minipoll.minipoll.R;
+import be.lsinf1225gr12.minipoll.minipoll.model.User;
 
-public class ChooseFriendAdapter extends ArrayAdapter<String> {
+public class ChooseFriendAdapter extends BaseAdapter {
+    /**
+     * Permet d'instancier un fichier xml de layout dans une vue.
+     */
+    private final LayoutInflater mInflater;
 
-    private final Context context;
-    private final String[] friends;
+    /**
+     * Liste des éléments de collection à mettre dans la liste.
+     */
+    private ArrayList<User> user;
 
-    public ChooseFriendAdapter(Context context, String[] f){
-        super(context,-1,f);
-        this.context=context;
-        this.friends=f;
+    public ChooseFriendAdapter(Context context, ArrayList<User> user) {
+        mInflater = LayoutInflater.from(context);
+        this.user = user;
     }
+
+    @Override
+    public int getCount() {
+        return user.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return user.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return user.get(i).getId();
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.adapter_choose_friend, parent, false);
-        TextView textView = (TextView) rowView.findViewById(R.id.name);
-        textView.setText(friends[position]);
+        // Si la vue n'a pas encore été créé (typiquement lors du première affichage de la liste).
+        // Android recycle en effet les layout déjà chargés des éléments de la liste (par exemple
+        // lors du changement de l'ordre dans la liste.)
 
-        return rowView;
+        if (convertView == null) {
+            // Création d'un nouvelle vue avec le layout correspondant au fichier xml
+            convertView = mInflater.inflate(R.layout.adapter_choose_friend, parent, false);
+        }
+
+        // Récupération des deux éléments de notre vue dans le but d'y placer les données.
+        TextView nameTextView = convertView.findViewById(R.id.show_row_name);
+
+        // Récupération et placement des données.
+        User myUser = user.get(position);
+        nameTextView.setText(myUser.getLogin());
+
+        return convertView;
+    }
+
+    /**
+     * Change la liste des éléments de collection affichée.
+     * <p>
+     * Permet de changer complètement la liste des éléments affichés dans la liste.
+     *
+     * @param newUser La nouvelle liste des éléments de collection à afficher.
+     */
+    public void setUser(ArrayList<User> newUser) {
+        this.user = user;
+        notifyDataSetChanged();
     }
 }
